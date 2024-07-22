b0a64c691cf24b0f9d083efa698f042e17877f52
Gabriel Soares <gabrielgyns@gmail.com>
2024-07-23 00:33:55 +0100

    auth with email and password, get profile

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
@@ -9,7 +10,9 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { authenticateWithPassword } from './routes/auth/authentication-with-password'
 import { createAccount } from './routes/auth/create-account'
+import { getProfile } from './routes/auth/get-profile'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -35,10 +38,15 @@ app.register(fastifySwaggerUI, {
 })
 
 // Registering
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
 app.register(fastifyCors)
 
 // Registering Routes
 app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
