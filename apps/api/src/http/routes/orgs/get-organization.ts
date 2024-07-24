0a5effceb7de96bed98123371be3cbf31735806f
Gabriel Soares <gabrielgyns@gmail.com>
2024-07-25 00:35:37 +0100

    orgs routes

---
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -0,0 +1,52 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get details of an organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            201: z.object({
+              organization: z.object({
+                id: z.string().uuid(),
+                name: z.string(),
+                slug: z.string(),
+                domain: z.string().nullable(),
+                shouldAttachUsersByDomain: z.boolean(),
+                avatarUrl: z.string().url().nullable(),
+                createdAt: z.date(),
+                updatedAt: z.date(),
+                ownerId: z.string().uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+
+        const { organization } = await request.getUserMembership(slug)
+
+        if (!organization) {
+          throw new BadRequestError('Organization not found.')
+        }
+
+        return { organization }
+      },
+    )
+}
